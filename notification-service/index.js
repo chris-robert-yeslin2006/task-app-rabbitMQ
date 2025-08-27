4454463357fd7cbc5fe18e76cc0ae6d2f658b3cb
chris-robert-yeslin2006 <robertchemist2006@gmail.com>
2025-08-27 20:30:01 +0530

    added retries in consumer service

--- notification-service/index.js
+++ notification-service/index.js
@@ -1,27 +1,66 @@
-const amqp=require('amqplib');
-const { connection } = require('mongoose');
+// const amqp=require('amqplib');
+// const { connection } = require('mongoose');
 
-async function start() {
-    try{
-        connection=await amqp.connect("amqp://rabbitmq");
-        channel=await connection.createChannel();
-        await channel.assertQueue("task_created");
-        console.log('Connected to RabbitMQ');
+// async function start() {
+//     try{
+//         connection=await amqp.connect("amqp://rabbitmq");
+//         channel=await connection.createChannel();
+//         await channel.assertQueue("task_created");
+//         console.log('Connected to RabbitMQ');
 
-        channel.consume("task_created",(msg)=>{
-            const taskData=JSON.parse(msg.content.toString());
-            console.log("Notification received for taskId:",taskData.title);
-            console.log(taskData);
-            channel.ack(msg);
-        });
-    }
-    catch(err){
-        console.error("Error connecting to RabbitMQ",err.message);
+//         channel.consume("task_created",(msg)=>{
+//             const taskData=JSON.parse(msg.content.toString());
+//             console.log("Notification received for taskId:",taskData.title);
+//             console.log(taskData);
+//             channel.ack(msg);
+//         });
+//     }
+//     catch(err){
+//         console.error("Error connecting to RabbitMQ",err.message);
 
-    }
+//     }
 
     
 
+// }
+
+// start();
+
+const amqp = require('amqplib');
+
+let connection;
+let channel;
+const queueName = "task_created";
+const rabbitUrl = "amqp://rabbitmq";
+const retryDelay = 5000; 
+
+async function connectRabbitMQ() {
+  try {
+    connection = await amqp.connect(rabbitUrl);
+    connection.on('error', (err) => {
+      console.error('RabbitMQ connection error:', err.message);
+    });
+    connection.on('close', () => {
+      console.error('RabbitMQ connection closed. Reconnecting...');
+      setTimeout(connectRabbitMQ, retryDelay);
+    });
+
+    channel = await connection.createChannel();
+    await channel.assertQueue(queueName);
+
+    console.log('Connected to RabbitMQ');
+
+    channel.consume(queueName, (msg) => {
+      const taskData = JSON.parse(msg.content.toString());
+      console.log("Notification received for taskId:", taskData.title);
+      console.log(taskData);
+      channel.ack(msg);
+    });
+  } catch (err) {
+    console.error('Error connecting to RabbitMQ:', err.message);
+    console.log(`Retrying connection in ${retryDelay / 1000} seconds...`);
+    setTimeout(connectRabbitMQ, retryDelay);
+  }
 }
 
-start();
+connectRabbitMQ();
